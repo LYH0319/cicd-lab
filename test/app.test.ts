a5db97d31f206f87db0aa1476f76a56570611815
LYH0319 <joannat512jb@gmail.com>
2026-05-23 06:18:00 +0000

    make test error

--- test/app.test.ts
+++ test/app.test.ts
@@ -21,7 +21,8 @@ describe('Fastify app', () => {
       url: '/'
     });
 
-    expect(response.statusCode).toBe(200);
+    //expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(100);
     expect(response.json().message).toBe('CI/CD Lab Fastify app is running');
     await app.close();
   });
